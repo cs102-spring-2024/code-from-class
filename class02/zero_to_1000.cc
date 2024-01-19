71854bdbad5d1fc8716d996cc543281f57a65af1
Tom Helmuth <thelmuth@hamilton.edu>
2024-01-19 16:40:55 +0000

    Code from class 02: 1/19/24

---
+++ class02/zero_to_1000.cc
@@ -0,0 +1,19 @@
+
+// This is the equivalent of an import statement
+#include <iostream>
+
+/*
+This is a multi-line
+comment!
+*/
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i <= 1000; i++) {
+        cout << i << " ";
+        cout << "Hello!";
+    }
+    cout << endl;
+    return 0;
+}

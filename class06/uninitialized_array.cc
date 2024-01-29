3ea8a4afa77f8d032d3e5dff678f34bf38ac4289
Tom Helmuth <thelmuth@hamilton.edu>
2024-01-29 11:06:47 -0500

    Code from class 6: 1/29/24

---
+++ class06/uninitialized_array.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int ARRAY_SIZE = 1000;
+
+int main() {
+
+    int grades[ARRAY_SIZE];
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        cout << "grades[" << i << "] = " << grades[i] << endl;
+    }
+
+    // cout << grades << endl;
+
+
+}
